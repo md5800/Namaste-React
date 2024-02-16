968d1ffe2c78f2b40ee97ef4d70962ff753914d6
md5800 <139066397+md5800@users.noreply.github.com>
2024-02-16 12:37:07 +0530

    Updated app.js till episode 3

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
-// const topHeading = React.createElement("h1", {id: "heading"}, "Hello from React");
-// const root = ReactDOM.createRoot(document.getElementById("root"));  
-// root.render(topHeading); 
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import logo from "./img/logo.jpg"
+import 'bootstrap/dist/css/bootstrap.css';
 
+const Header =() => {
+    return (
+        <div className="headersec">
+            <div className="container">
+                <div className="row justify-content-between align-items-center">
+                    <div className="col-md-3">
+                        <div className="logo">
+                            <img src={logo} alt="logo" />
+                        </div>
+                    </div>
+                    <div className="col-md-9">
+                        <div className="navitem">
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+const Body =() =>{
+    return (
+        <div className="mainsec">
+            <div className="container">
+                <div className="searchbox">Search</div>
+            </div>
+            <div className="cardsec">
+                <div className="container">
+                    <div className="cardbox">
+                        <h1>Card</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
-const parentDiv = React.createElement( "div", {className: "parentDiv"}, 
-[
-    React.createElement( "div", {className: "childDiv1"}, 
-    [React.createElement( "h1", {}, "H1 tag from childDiv1"),React.createElement( "h2", {}, "H2 tag from childDiv1")]
-    ),
-    React.createElement( "div", {className: "childDiv2"}, 
-    [React.createElement( "h1", {}, "H1 tag from childDiv2"),React.createElement( "h2", {}, "H2 tag from childDiv2")]
-    )
-]
-)
-
+const AppComponent = () =>{
+return (
+    <div className="app">
+        <Header />
+        <div className="bodywarpper">
+          <Body />
+        </div>
+    </div>
+);
+};
 const container = ReactDOM.createRoot(document.getElementById("root"));
-container.render(parentDiv);
+container.render(<AppComponent/>);
